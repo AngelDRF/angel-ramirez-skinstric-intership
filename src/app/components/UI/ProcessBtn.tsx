360e17182a1bc6952a3a422544051af5bb725da6
Angel Ramirez <dgju04@gmail.com>
2025-09-30 12:28:51 -0400

    Full phase-one working, also added animations for pages

---
+++ src/app/components/UI/ProcessBtn.tsx
@@ -0,0 +1,23 @@
+const ProcessBtn = () => {
+  return (
+    <div>
+      <div className=" w-12 h-12 flex items-center justify-center border border-[#1A1B1C] rotate-45 scale-[1] sm:hidden">
+        <span className="rotate-[-45deg] text-xs font-semibold sm:hidden">
+          PROCEED
+        </span>
+      </div>
+
+      <div className="group hidden sm:flex flex-row relative justify-center items-center">
+        <span className="text-sm font-semibold hidden sm:block mr-5">
+          PROCEED
+        </span>
+        <div className=" w-12 h-12 hidden sm:flex justify-center border border-[#1A1B1C] rotate-45 scale-[0.85] group-hover:scale-[0.92] ease duration-300"></div>
+        <span className="absolute right-[15px] bottom-[13px] scale-[0.9] hidden sm:block group-hover:scale-[0.92] ease duration-300">
+          ▶
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default ProcessBtn;
