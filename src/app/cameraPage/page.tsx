c7f84ec360e3d1ecb5c006a384219d45ef26d479
Angel Ramirez <dgju04@gmail.com>
2025-10-01 18:39:38 -0400

    Starting to work on phase-three requirements, new features added

---
+++ src/app/cameraPage/page.tsx
@@ -0,0 +1,11 @@
+import CameraLoading from "../cameraUtilities/CameraLoading";
+
+const CameraPage = () => {
+  return (
+    <div className="md:h-[85vh] h-[65vh] bg-white flex items-center justify-center">
+      <CameraLoading />
+    </div>
+  );
+};
+
+export default CameraPage;
